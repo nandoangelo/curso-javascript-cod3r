25e217c2d30471cfffe3e84279750afe2484bd99
Fernando Angelo <nandoangelo@gmail.com>
2023-02-01 17:17:35 -0300

    Entendendo o Hoisting
    Exemplo de como o içamento da variável acontece.

---
+++ 04-fundamentos-js/59-hoisting.js
@@ -0,0 +1,19 @@
+/*
+* Cod3r - Udemy
+* Seção 04 - Fundamentos do JavaScript
+* Aula 59 - Entendendo o Hoisting (içamento)
+*/
+
+
+console.log('a = ', a) // a = undefined
+var a = 2
+console.log('a = ', a) // a = 2
+
+/**
+ * EXPLICANDO: 
+ * O interpretador consegue içar a variável declarada
+ * para o topo e depois, mais abaixo, que vai atribuir
+ * valor a ela.
+ * 
+ * Com o 'let' esse efeito de içamento não ocorre
+ */
--- 04-fundamentos-js/error.js
+++ 04-fundamentos-js/error.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+// armazena numero sorteado entre 0 e 1000
+const numeroSecreto = Math.floor(Math.random() * 100)
+
+let palpite = prompt('Pensei em um número entre 0 e 100. Quer arriscar um palpite de qual ele é?')
+
+palpite = Number(palpite)
+
+for (let i = 0; i < 3; i++)
+{
+  if (palpite == numeroSecreto) {
+    console.log('VOCÊ ACERTOU!!')
+    break
+  }
+  else if (palpite > numeroSecreto) {
+    console.log('Você chutou alto. Seu palpite é maior do que o número que pensei')
+    palpite = prompt('Tente novamente')
+  }
+  else {
+    console.log('Você chutou baixo. Seu palpite é menor do que o número que pensei')
+  }
+  console.log(`Essa foi sua ${i+1}ª tentativa.`)
+}
+
+console.log(`O numero sorteado foi: ${numeroSecreto}`)
